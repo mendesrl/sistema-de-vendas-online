cfd3caf164934aeb390cee3745b99a57fb7f3b82
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-07 19:40:14 -0400

    rename file Validate to ValidateCpf

--- src/ValidateCpf.ts
+++ src/ValidateCpf.ts
@@ -1,18 +1,15 @@
-export default class Validate {
-  constructor(readonly cpf: string) {
-    if (!this.isValid(cpf)) throw new Error("O CPF é invalido");
-  }
-
-  isValid(cpf: string) {
-    if (cpf !== null && cpf !== undefined) {
-      if (cpf.length >= 11 || cpf.length <= 14) {
-        cpf = cpf
+// @ts-nocheck
+export function ValidateCpf(str) {
+  if (str !== null) {
+    if (str !== undefined) {
+      if (str.length >= 11 || str.length <= 14) {
+        str = str
           .replace(".", "")
           .replace(".", "")
           .replace("-", "")
           .replace(" ", "");
 
-        if (!cpf.split("").every((c) => c === cpf[0])) {
+        if (!str.split("").every((c) => c === str[0])) {
           try {
             let d1, d2;
             let dg1, dg2, rest;
@@ -21,11 +18,16 @@ export default class Validate {
             d1 = d2 = 0;
             dg1 = dg2 = rest = 0;
 
-            for (let nCount = 1; nCount < cpf.length - 1; nCount++) {
-              digito = parseInt(cpf.substring(nCount - 1, nCount));
+            for (let nCount = 1; nCount < str.length - 1; nCount++) {
+              // if (isNaN(parseInt(str.substring(nCount -1, nCount)))) {
+              // 	return false;
+              // } else {
+
+              digito = parseInt(str.substring(nCount - 1, nCount));
               d1 = d1 + (11 - nCount) * digito;
 
               d2 = d2 + (12 - nCount) * digito;
+              // }
             }
 
             rest = d1 % 11;
@@ -36,14 +38,16 @@ export default class Validate {
             if (rest < 2) dg2 = 0;
             else dg2 = 11 - rest;
 
-            let nDigVerific = cpf.substring(cpf.length - 2, cpf.length);
+            let nDigVerific = str.substring(str.length - 2, str.length);
             nDigResult = "" + dg1 + "" + dg2;
             return nDigVerific == nDigResult;
           } catch (e) {
+            console.error("Erro !" + e);
+
             return false;
           }
         } else return false;
       } else return false;
-    } else return false;
-  }
+    }
+  } else return false;
 }
