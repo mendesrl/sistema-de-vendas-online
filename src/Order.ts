ffacde8a53b31c43d6404ea898b415fcbcc848e2
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-08 18:11:20 -0400

    Feat: config data base

--- src/Order.ts
+++ src/Order.ts
@@ -1,4 +1,29 @@
 import Product from "./Product";
+import express, {Request, Response} from "express";
+import { ValidateCpf } from "./ValidateCpf";
+import pgp from 'pg-promise'
+
+const app = express();
+app.use(express.json());
+
+app.post("/checkout",  async function (req: Request, res: Response) {
+  
+  if(ValidateCpf(req.body.cpf)) {
+    const connection = pgp()("postgres://postgres:Postgres2023!@localhost:5432/cccat11")
+    if(req.body.items) {
+      for (const item of req.body.items) { 
+        const [productData] = await connection.query("select * from cccat11.product where id_product = $1",[item.id_product]);
+        console.log(productData)
+      }
+    }
+    res.end();
+  }
+  else {
+    res.json({message: "Invalid CPF"})
+  }
+})
+console.log('ouvindo');
+app.listen(3001);
 
 export default class Order {
   private products: Product[];
