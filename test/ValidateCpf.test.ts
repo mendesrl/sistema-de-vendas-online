73f66ae9ff440e14c970b5dda792b33842442648
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-07 20:33:05 -0400

    add case test to validate CPF

--- test/ValidateCpf.test.ts
+++ test/ValidateCpf.test.ts
@@ -1,13 +1,33 @@
 import { ValidateCpf } from "../src/ValidateCpf";
 
-test("Deve testar um CPF válido", () => {
-  const isValid = ValidateCpf("041.273.711-61");
+test.each([
+  "041.273.711-61",
+  "356.191.631-72",
+  "447.118.751-15"
+])("Should be valid CPF: %s", (cpf:string) => {
+  const isValid = ValidateCpf(cpf);
 
   expect(isValid).toBeTruthy();
 });
 
-test('Deve testar um CPF inválido', () =>{
-    const isValid = ValidateCpf("041-7648-09");
 
-    expect(isValid).toBeFalsy();
-})
+test.each([
+  "041.273.711-00",
+  "041.273.711-01",
+  "447-118-751.15",
+  "447.118"
+])("Should be invalid CPF: %s", (cpf:string) => {
+  const isValid = ValidateCpf(cpf);
+
+  expect(isValid).toBeFalsy();
+});
+
+test.each([
+  "111.111.111-11",
+])("Should be invalid if numbers %s are the same", (cpf:string) => {
+  const isValid = ValidateCpf(cpf);
+
+  expect(isValid).toBeFalsy();
+});
+  
+
