5adfa62779b7e7fc2dd63842af315235a4684d1c
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-07 21:03:00 -0400

    Refactoring: removing bad error handling

--- src/ValidateCpf.ts
+++ src/ValidateCpf.ts
@@ -2,36 +2,32 @@ export function ValidateCpf(str: string) {
   if (!str) return false;
   str = str.replace(".", "").replace(".", "").replace("-", "").replace(" ", "");
   if (str.length !== 11) return false;
-  const allDigitsEqual = !str.split("").every((c) => c === str[0])
-  if (allDigitsEqual) {
-    try {
-      let d1, d2;
-      let dg1, dg2, rest;
-      let digito;
-      let nDigResult;
-      d1 = d2 = 0;
-      dg1 = dg2 = rest = 0;
+  const allDigitsEqual = !str.split("").every((c) => c === str[0]);
+  if (!allDigitsEqual) return false;
 
-      for (let nCount = 1; nCount < str.length - 1; nCount++) {
-        digito = parseInt(str.substring(nCount - 1, nCount));
-        d1 = d1 + (11 - nCount) * digito;
+  let d1, d2;
+  let dg1, dg2, rest;
+  let digito;
+  let nDigResult;
+  d1 = d2 = 0;
+  dg1 = dg2 = rest = 0;
 
-        d2 = d2 + (12 - nCount) * digito;
-      }
+  for (let nCount = 1; nCount < str.length - 1; nCount++) {
+    digito = parseInt(str.substring(nCount - 1, nCount));
+    d1 = d1 + (11 - nCount) * digito;
 
-      rest = d1 % 11;
+    d2 = d2 + (12 - nCount) * digito;
+  }
 
-      dg1 = rest < 2 ? (dg1 = 0) : 11 - rest;
-      d2 += 2 * dg1;
-      rest = d2 % 11;
-      if (rest < 2) dg2 = 0;
-      else dg2 = 11 - rest;
+  rest = d1 % 11;
 
-      let nDigVerific = str.substring(str.length - 2, str.length);
-      nDigResult = "" + dg1 + "" + dg2;
-      return nDigVerific == nDigResult;
-    } catch (e) {
-      return false;
-    }
-  } else return false;
+  dg1 = rest < 2 ? (dg1 = 0) : 11 - rest;
+  d2 += 2 * dg1;
+  rest = d2 % 11;
+  if (rest < 2) dg2 = 0;
+  else dg2 = 11 - rest;
+
+  let nDigVerific = str.substring(str.length - 2, str.length);
+  nDigResult = "" + dg1 + "" + dg2;
+  return nDigVerific == nDigResult;
 }
