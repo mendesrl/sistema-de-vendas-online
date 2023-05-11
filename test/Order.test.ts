b14d89ea87ba9d394f0affaa1e7b2cdeaf4c1ab5
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-11 09:21:10 -0400

    Feat: add test to invalid coupon

--- test/Order.test.ts
+++ test/Order.test.ts
@@ -11,17 +11,8 @@ test("shouldn't create new order if CPF is invalid", async function() {
   expect(output.message).toBe('Invalid CPF')
 })
 
-test("shouldn't create new order if CPF is invalid", async function() {
-  const input = {
-    cpf:"041.273.711-00"
-  }
-  const response = await axios.post('http://localhost:3001/checkout', input)
-  const output= response.data;
 
-  expect(output.message).toBe('Invalid CPF')
-})
-
-test("should be calculate an order with 3 products", async function() {
+test("Should be calculate an order with 3 products", async function() {
   const input = {
     cpf:"041.273.711-61",
     items: [
@@ -36,7 +27,37 @@ test("should be calculate an order with 3 products", async function() {
   expect(output.total).toBe(6090)
 })
 
+test("Should be calculate an order with 3 products with discount coupon", async function() {
+  const input = {
+    cpf:"041.273.711-61",
+    items: [
+      {id_product: 1, qtd: 1 }, //1000
+      {id_product: 2, qtd: 1 }, //5000
+      {id_product: 3, qtd: 3 } //30
+    ],
+    coupon: "VALE20"
+  }
+  const response = await axios.post('http://localhost:3001/checkout', input)
+  const output= response.data;
 
+  expect(output.total).toBe(4872)
+})
+
+test("Shouldn't be applied to the order an expired discount coupon", async function() {
+  const input = {
+    cpf:"041.273.711-61",
+    items: [
+      {id_product: 1, qtd: 1 }, //1000
+      {id_product: 2, qtd: 1 }, //5000
+      {id_product: 3, qtd: 3 } //30
+    ],
+    coupon: "VALE10"
+  }
+  const response = await axios.post('http://localhost:3001/checkout', input)
+  const output= response.data;
+
+  expect(output.message).toBe('Invalid Coupon')
+})
 
 // test("should be calculate the total value of a product", function () {
 //   const order = new Order();
