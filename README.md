074fe7de12773d5ddb0637efde902e73cbd660a6
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-15 15:57:45 -0400

    Feat: add tests for dimensions

--- README.md
+++ README.md
@@ -30,8 +30,8 @@ $  npx jest
 - [x] Ao fazer um pedido, o mesmo item não pode ser informado mais de uma vez
 - [x] Deve calcular o valor do frete com base nas dimensões (altura, largura e profundidade em cm) e o peso dos produtos (em kg)
 - [x] Deve retornar o preço mínimo de frete caso ele seja superior ao valor calculado
-- [] Nenhuma dimensão do item pode ser negativa
-- [] O peso do item não pode ser negativo
+- [x] Nenhuma dimensão do item pode ser negativa
+- [x] O peso do item não pode ser negativo
 
 
 ### Autora
--- src/Order.ts
+++ src/Order.ts
@@ -12,57 +12,61 @@ app.post("/checkout", async function (req: Request, res: Response) {
     "postgres://postgres:Postgres2023!@localhost:5432/cccat11"
   );
   try {
-    if (ValidateCpf(req.body.cpf)) {
-      const output = {
-        subtotal: 0,
-        total: 0,
-        freight: 0,
-      };
-      if (req.body.items) {
-        for (const item of req.body.items) {
-          if (item.qtd <= 0) throw new Error("Invalid quantity");
-          if (
-            req.body.items.filter((i: any) => i.id_product === item.id_product)
-              .length > 1
-          )
-            throw new Error("Duplicated Item");
+    if (!ValidateCpf(req.body.cpf)) throw new Error("Invalid CPF");
+    const output = {
+      subtotal: 0,
+      total: 0,
+      freight: 0,
+    };
+    if (req.body.items) {
+      for (const item of req.body.items) {
+        if (item.qtd <= 0) throw new Error("Invalid quantity");
+        if (
+          req.body.items.filter((i: any) => i.id_product === item.id_product)
+            .length > 1
+        )
+          throw new Error("Duplicated Item");
 
-          const [productData] = await connection.query(
-            "select * from cccat11.product where id_product = $1",
-            [item.id_product]
-          );
-          const price = parseFloat(productData.price);
-          output.subtotal += price * item.qtd;
-          if (req.body.from && req.body.to) {
-            const volume =
-              ((((productData.width / 100) * productData.height) / 100) *
-                productData.length) /
-              100;
-            const density = parseFloat(productData.weight) / volume;
-            let freight = volume * 1000 * (density / 100);
-            freight = Math.max(10,freight);
-            output.freight += freight * item.qtd;
-          }
+        const [productData] = await connection.query(
+          "select * from cccat11.product where id_product = $1",
+          [item.id_product]
+        );
+        if (
+          productData.width <= 0 ||
+          productData.height <= 0 ||
+          productData.length <= 0 ||
+          productData.weight <= 0
+        )
+          throw new Error("Invalid dimensions");
+        const price = parseFloat(productData.price);
+        output.subtotal += price * item.qtd;
+        if (req.body.from && req.body.to) {
+          const volume =
+            ((((productData.width / 100) * productData.height) / 100) *
+              productData.length) /
+            100;
+          const density = parseFloat(productData.weight) / volume;
+          let freight = volume * 1000 * (density / 100);
+          freight = Math.max(10, freight);
+          output.freight += freight * item.qtd;
         }
-        output.total = output.subtotal;
-        if (req.body.coupon) {
-          const [couponData] = await connection.query(
-            "select * from cccat11.coupon where code = $1",
-            [req.body.coupon]
-          );
-          if (couponData && ValidateCoupon(couponData.expired)) {
-            output.total -=
-              (output.total * parseFloat(couponData.percentage)) / 100;
-          } else {
-            res.json({ message: "Invalid Coupon" });
-            return;
-          }
-        }
-        output.total += output.freight;
-        res.json(output);
-      } else {
-        res.json({ message: "Invalid CPF" });
       }
+      output.total = output.subtotal;
+      if (req.body.coupon) {
+        const [couponData] = await connection.query(
+          "select * from cccat11.coupon where code = $1",
+          [req.body.coupon]
+        );
+        if (couponData && ValidateCoupon(couponData.expired)) {
+          output.total -=
+            (output.total * parseFloat(couponData.percentage)) / 100;
+        } else {
+          res.json({ message: "Invalid Coupon" });
+          return;
+        }
+      }
+      output.total += output.freight;
+      res.json(output);
     }
   } catch (error: any) {
     res.status(422).json({ message: error.message });
