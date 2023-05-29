eceb495ae4e26889b31c08fd30d2e14c79c80217
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-28 21:40:56 -0400

    Feat: add test to simulate freight

---
+++ test/SimulateFreight.test.ts
@@ -0,0 +1,18 @@
+import ProductRepositoryDatabase from "../src/ProductRepositoryDatabase";
+import SimulateFreight from "../src/SimulateFreight";
+
+test('Should be simulated freight', async () => {
+    const input = {
+        items: [
+          { id_product: 1, qtd: 1 },
+          { id_product: 2, qtd: 1 },
+          { id_product: 3, qtd: 3 },
+        ],
+        from: "88015600",
+        to: "22030600",
+      };
+    const productRepository = new ProductRepositoryDatabase();
+    const simulateFreight = new SimulateFreight(productRepository);
+    const output = await simulateFreight.execute(input);
+    expect(output.freight).toBe(280)
+})
