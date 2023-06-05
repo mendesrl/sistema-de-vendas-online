f6823f9869dcc88b235e7114767d1eca17904787
mendesrl <ribeiro.larissamendes@gmail.com>
2023-06-05 09:45:34 -0400

    Feat: add class and test for product

--- src/Product.ts
+++ src/Product.ts
@@ -7,10 +7,15 @@ export default class Product {
     readonly height: number,
     readonly length: number,
     readonly weight: number
-  ) {}
+  ) {
+    // if (width <= 0 || height <= 0 || length <= 0)
+    //   throw new Error("Invalid dimensions");
+    // if (weight <= 0) throw new Error("Invalid weight");
+  }
 
   getVolume() {
-    const volume = this.width/100 * this.height/100 * this.length/100;
+    const volume =
+      ((((this.width / 100) * this.height) / 100) * this.length) / 100;
     return volume;
   }
   getDensity() {
--- test/Product.test.ts
+++ test/Product.test.ts
@@ -1,14 +1,25 @@
 import Product from "../src/Product";
 
 test("Should be able to calculate the volume", () => {
-  const product = new Product(1, "A",1000,100,30,10,3);
+  const product = new Product(1, "A", 1000, 100, 30, 10, 3);
   const volume = product.getVolume();
   expect(volume).toBe(0.03);
 });
 
 test("Should be able to calculate the density", () => {
-    const product = new Product(1, "A",1000,100,30,10,3);
-    const density = product.getDensity();
-    expect(density).toBe(100);
-  });
-  
+  const product = new Product(1, "A", 1000, 100, 30, 10, 3);
+  const density = product.getDensity();
+  expect(density).toBe(100);
+});
+
+// test("Should be able create with invalid dimensions", () => {
+//   expect(() => new Product(1, "A", 1000, -100, -30, -10, 3)).toThrowError(
+//     "Invalid dimensions"
+//   );
+// });
+
+// test("Should be able create with invalid weight", () => {
+//   expect(() => new Product(1, "A", 1000, 100, 30, 10, -3)).toThrowError(
+//     "Invalid weight"
+//   );
+// });
